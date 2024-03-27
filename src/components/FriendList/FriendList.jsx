ca2a26858fae9e522ec3a7a2a3ab1a3863fba11c
Serhii-Yakimtso <serafyso@gmail.com>
2024-03-28 00:50:54 +0200

    add *.jsx for task 1 & 2

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,17 @@
+import FriendListItem from '../FriendListItem/FriendListItem';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul>
+      {friends.map(friend => {
+        return (
+          <li key={friend.id}>
+            <FriendListItem friend={friend} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default FriendList;
