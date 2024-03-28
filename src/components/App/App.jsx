2ed5e19dd88faef06cce86e647437f149549e995
Serhii-Yakimtso <serafyso@gmail.com>
2024-03-29 00:02:28 +0200

    add task #3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,9 @@
 import Profile from '../Profile/Profile';
 import FriendList from '../FriendList/FriendList';
+import TransactionHistory from '../TransactionHistory/TransactionHistory';
 import userData from '../../userData.json';
 import friends from '../../friends.json';
+import transactions from '../../transactions.json';
 import './App.css';
 
 function App() {
@@ -15,6 +17,7 @@ function App() {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 }
